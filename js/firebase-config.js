e7042dfe4f9fa51a84175726702b86c1f49dbd4d
Star Varse <starrvarse@gmail.com>
2024-10-04 10:17:24 +0530

    firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,12 +1,12 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCNCxaepUbONLg-_z0IEw8ovtMnUPEZk7k",
-    authDomain: "employeemanagement-2c6ff.firebaseapp.com",
-    projectId: "employeemanagement-2c6ff",
-    storageBucket: "employeemanagement-2c6ff.appspot.com",
-    messagingSenderId: "597841526423",
-    appId: "1:597841526423:web:c55d2f8e569b6c68caf3d2",
-    measurementId: "G-KJV8W4JEB9"
+    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    authDomain: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    projectId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    storageBucket: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    messagingSenderId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    measurementId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 };
 
 // Initialize Firebase
